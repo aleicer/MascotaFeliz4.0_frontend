7b4e8d1017afef4713123d5f176e253c7709d4f9
Aleicer <aleicer.vesga@gmail.com>
2021-11-23 21:14:18 -0500

    proyecto modificado, se agrega espacion para pie de pagina, barra navegacion, se agrega conexion a estilos materialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { CrearClienteComponent } from './clientes/crear-cliente/crear-cliente.component';
+import { EditarClienteComponent } from './clientes/editar-cliente/editar-cliente.component';
+import { BuscarClienteComponent } from './clientes/buscar-cliente/buscar-cliente.component';
+import { EliminarClienteComponent } from './clientes/eliminar-cliente/eliminar-cliente.component';
+import { BarraNavegacionComponent } from './nav-footer/barra-navegacion/barra-navegacion.component';
+import { PiePaginaComponent } from './nav-footer/pie-pagina/pie-pagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientesComponent,
+    CrearClienteComponent,
+    EditarClienteComponent,
+    BuscarClienteComponent,
+    EliminarClienteComponent,
+    BarraNavegacionComponent,
+    PiePaginaComponent
   ],
   imports: [
     BrowserModule,
