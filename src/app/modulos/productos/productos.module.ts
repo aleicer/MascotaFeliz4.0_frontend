a4b789af9ce95124ad2dd7b7faf45e1dc72d45b8
Daniela Becerra <danielabecerrasosa@gmail.com>
2021-12-04 21:15:55 -0500

    creacion de modelo Planes

--- src/app/modulos/productos/productos.module.ts
+++ src/app/modulos/productos/productos.module.ts
@@ -3,11 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { ProductosRoutingModule } from './productos-routing.module';
 import { CrearProductoComponent } from './crear-producto/crear-producto.component';
+import { PlanesComponent } from './planes/planes.component';
+import { CrearPlanComponent } from './planes/crear-plan/crear-plan.component';
+import { ListarPlanComponent } from './planes/listar-plan/listar-plan.component';
+import { BuscarPlanComponent } from './planes/buscar-plan/buscar-plan.component';
+import { EditarPlanComponent } from './planes/editar-plan/editar-plan.component';
+import { EliminarPlanComponent } from './planes/eliminar-plan/eliminar-plan.component';
 
 
 @NgModule({
   declarations: [
-    CrearProductoComponent
+    CrearProductoComponent,
+    PlanesComponent,
+    CrearPlanComponent,
+    ListarPlanComponent,
+    BuscarPlanComponent,
+    EditarPlanComponent,
+    EliminarPlanComponent
   ],
   imports: [
     CommonModule,
