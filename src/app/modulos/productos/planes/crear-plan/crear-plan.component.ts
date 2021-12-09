2598047667839bc5f6fa9a6354ac399e6869884b
Daniela Becerra <danielabecerrasosa@gmail.com>
2021-12-09 07:25:07 -0500

    modificacion del codigo crear-plan component.ts

--- src/app/modulos/productos/planes/crear-plan/crear-plan.component.ts
+++ src/app/modulos/productos/planes/crear-plan/crear-plan.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ModelPlan } from 'src/app/modelos/plan.model';
+import { ClienteService } from 'src/app/servicios/cliente.service';
 
 @Component({
   selector: 'app-crear-plan',
@@ -6,10 +10,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crear-plan.component.css']
 })
 export class CrearPlanComponent implements OnInit {
+  fgValidador: FormGroup = this.fb.group({
+    'nombres':['',[Validators.required]],
+    'apellidos':['',[Validators.required]],
+    'documento':['',[Validators.required]],
+    'sexo':['',[Validators.required]],
+    'direccion':['',[Validators.required]],
+    'telefono':['',[Validators.required]],
+    'ciudad':['',[Validators.required]],
+    'clave':['',[Validators.required]],
+    'correo':['',[Validators.required]],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder,
+    private servicioCliente: ClienteService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  GuardarCliente(){
+    let c = new ModelPlan();
+    let telefono = this.fgValidador.controls["telefono"].value;
+    let documento = this.fgValidador.controls["documento"].value;
+    c.nombre= this.fgValidador.controls["nombre"].value;
+    c.descripcion = this.fgValidador.controls["descripcion"].value;
+    c.precio = this.fgValidador.controls["precio"].value;
+    
+    this.servicioCliente.CrearCliente(c).subscribe((datos: ModelPlan)=>{
+      alert("Usuario Creado con ID "+ datos.id);
+      this.router.navigate(["/administracion/buscar-clientes"]);
+    }),(Error : any)=>{
+      alert("Error al crear Usuario");
+    }
+
+  }
 }
