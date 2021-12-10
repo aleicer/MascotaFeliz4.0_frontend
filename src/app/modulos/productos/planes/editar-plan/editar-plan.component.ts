9fa55896421220f828cd84c2426597c29325fb31
Daniela Becerra <danielabecerrasosa@gmail.com>
2021-12-10 15:33:50 -0500

    Editar Planes

--- src/app/modulos/productos/planes/editar-plan/editar-plan.component.ts
+++ src/app/modulos/productos/planes/editar-plan/editar-plan.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ModelPlan } from 'src/app/modelos/plan.model';
+import { PlanesService } from 'src/app/servicios/planes.service';
 
 @Component({
   selector: 'app-editar-plan',
@@ -7,9 +11,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarPlanComponent implements OnInit {
 
-  constructor() { }
+  id: string = '';
+  fgValidador: FormGroup = this.fb.group({
+    'nombre':['',[Validators.required]],
+    'descripcion':['',[Validators.required]],
+    'precio':['',[Validators.required]],
+    });
+
+  constructor(private fb: FormBuilder,
+    private servicioPlan: PlanesService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params["id"];
+    this.BuscarPlan();
+  }
+
+  BuscarPlan(){
+    this.servicioPlan.ObtenerRegistroPorId(this.id).subscribe((datos: ModelPlan) => {
+      this.fgValidador.controls["id"].setValue(this.id);
+      this.fgValidador.controls["nombre"].setValue(datos.nombre);
+      this.fgValidador.controls["descripcion"].setValue(datos.descripcion);
+      this.fgValidador.controls["Precio"].setValue(datos.precio);})
+    })
+  }
+  EditarPlan(){
+    let nombre = this.fgValidador.controls["nombre"].value;
+    let descripcion = this.fgValidador.controls["Descripcion"].value;
+    let precio = this.fgValidador.controls["Precio"].value;
+    let p = new ModelPlan();
+    p.nombre = nombre;
+    p.descripcion = descripcion;
+    p.precio = precio;
+    p.id = this.id;
+
+    this.servicioPlan.ActualizarPlan(p).subscribe((datos: ModelPlan) => {
+      alert("Empleado actualizado correctamente");
+      this.router.navigate(["/administracion/listar-plan"]);
+    },(error: any) => {
+      alert("Error editando el plan");
+    })
   }
 
 }
